cc7c972ae2030d5add28113fd87de694eedea3b0
androidiraqloves-beep <android.iraq.loves@gmail.com>
2026-06-10 01:43:50 +0300

    Add files via upload

---
+++ apps/RepFlow/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'repflow-ios-pwa-logo-v2';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './repflow-logo.png',
+  './logo.png',
+  './apple-touch-icon.png',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icons/apple-touch-icon-180.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './offline.html',
+  './admin.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+      return res;
+    }).catch(() => caches.match('./index.html').then(res => res || caches.match('./offline.html',
+  './admin.html'))));
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(() => caches.match('./offline.html',
+  './admin.html')))
+  );
+});
